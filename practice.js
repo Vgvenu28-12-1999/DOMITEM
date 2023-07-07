c9723d4bdf634826ed8d2c9e51c84fe7974e0792
Vgvenu28-12-1999 <89677083+Vgvenu28-12-1999@users.noreply.github.com>
2023-07-07 07:00:30 +0530

    add-product(express)

--- practice.js
+++ practice.js
@@ -1,5 +1,26 @@
-const http = require('http');
-const route = require('./route');
-console.log(route.someText)
-const server = http.createServer(route.handler);
-server.listen(5015);
+
+const express= require('express');
+const bodyParse = require('body-parser');
+const practice = express();
+practice.use(bodyParse.urlencoded({extended: false}));
+
+
+practice.use( '/add-product',(req,res,next)=>{
+  
+    res.send('<form action ="/product" method="POST"><input type="text" name="title" ><input type="number" number="size"><button type="submit"> Add product </button></form> ');
+
+});
+practice.post( '/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+
+});
+practice.use('/',(req,res,next)=>{
+    
+    res.send('<h1>Hello from express</h1>')
+});
+
+practice.listen(6005);
+
+
+
